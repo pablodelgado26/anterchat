d604ae1e65f0586ac7670250e1d036f362342930
Felipe Santos <dev.felipesantos@gmail.com>
2026-01-15 21:57:28 -0300

    feat: prepara build

--- android/app/src/main/java/com/antera/chat/MainActivity.kt
+++ android/app/src/main/java/com/antera/chat/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.anteraapp
+package com.antera.chat
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/antera/chat/MainApplication.kt
+++ android/app/src/main/java/com/antera/chat/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.anteraapp
+package com.antera.chat
 
 import android.app.Application
 import android.content.res.Configuration
